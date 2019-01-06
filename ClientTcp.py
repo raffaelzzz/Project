812159441fbaf53bfc8298f0483f23d0e4c64ea1
raffaelzzz <34611785+raffaelzzz@users.noreply.github.com>
2019-01-05 23:33:38 -0200

    Update ClientTcp.py

--- ClientTcp.py
+++ ClientTcp.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 
-import socket // Importando o SOCKET
+import socket #// Importando o SOCKET
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) // CRIANDO O SOCKET COM ESTRUTURA IPV4 e TCP/IP
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #// CRIANDO O SOCKET COM ESTRUTURA IPV4 e TCP/IP
 
-client.settimeout(15) //O TEMPO QUE O CLIENTE VAI SE CONECTAR COM O SERVIDOR (15) SE PASSAR DISSO ELE CANCELA
+client.settimeout(15) #//O TEMPO QUE O CLIENTE VAI SE CONECTAR COM O SERVIDOR (15) SE PASSAR DISSO ELE CANCELA
 
-try: //Então
+try: #//Então
     
-    client.connect(('www.institutodeandhela.com.br', 80)) //ESTOU DANDO O CONNECT PARA ELE INICIAR, DANDO TAMBÉM OS PARAMENTROS
+    client.connect(('www.institutodeandhela.com.br', 80))# //ESTOU DANDO O CONNECT PARA ELE INICIAR, DANDO TAMBÉM OS PARAMENTROS
     
-    print client.send("GET / HTTP/1.1\nHost: institutodeandhela.com.br\n\n\n")//A MENSAGEM QUE VAMOS ENVIAR PARA O SERVIDOR
+    print client.send("GET / HTTP/1.1\nHost: institutodeandhela.com.br\n\n\n")#//A MENSAGEM QUE VAMOS ENVIAR PARA O SERVIDOR
     
-    print client.recv(1024) // MENSAGEM QUE VAMOS RECEBER, 1024 BUFFEROVERFLOW
+    print client.recv(1024)# // MENSAGEM QUE VAMOS RECEBER, 1024 BUFFEROVERFLOW
 
-except: // SENÃO
+except:# // SENÃO
     print "A conexão falhou"
 
 
