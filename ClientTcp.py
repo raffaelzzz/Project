2d66fd00deaf59464e48516c13967838e5fa5045
raffaelzzz <34611785+raffaelzzz@users.noreply.github.com>
2019-01-05 23:32:23 -0200

    Update ClientTcp.py

--- ClientTcp.py
+++ ClientTcp.py
@@ -1,17 +1,20 @@
 # -*- coding: utf-8 -*-
 
-import socket
+import socket // Importando o SOCKET
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) // CRIANDO O SOCKET COM ESTRUTURA IPV4 e TCP/IP
 
-client.settimeout(15)
+client.settimeout(15) //O TEMPO QUE O CLIENTE VAI SE CONECTAR COM O SERVIDOR (15) SE PASSAR DISSO ELE CANCELA
 
-try:
-    client.connect(('www.institutodeandhela.com.br', 80))
-    print client.send("GET / HTTP/1.1\nHost: institutodeandhela.com.br\n\n\n")
-    print client.recv(1024)
+try: //Então
+    
+    client.connect(('www.institutodeandhela.com.br', 80)) //ESTOU DANDO O CONNECT PARA ELE INICIAR, DANDO TAMBÉM OS PARAMENTROS
+    
+    print client.send("GET / HTTP/1.1\nHost: institutodeandhela.com.br\n\n\n")//A MENSAGEM QUE VAMOS ENVIAR PARA O SERVIDOR
+    
+    print client.recv(1024) // MENSAGEM QUE VAMOS RECEBER, 1024 BUFFEROVERFLOW
 
-except:
+except: // SENÃO
     print "A conexão falhou"
 
 
