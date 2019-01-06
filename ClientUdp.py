a5f49c0c3e750d5638fdf19167d43282ec885943
raffaelzzz <34611785+raffaelzzz@users.noreply.github.com>
2019-01-05 23:40:47 -0200

    Update ClientUdp.py

--- ClientUdp.py
+++ ClientUdp.py
@@ -2,23 +2,25 @@
 
 import socket
 
-client_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #//CRIANDO O SOCKET UDP E UTILIZANDO O IPV4
 
 
-try:
+try: #// então
 
 
-    while True:
-        client_udp.sendto(raw_input("Digite sua mensagem: "), ("127.0.0.1", 123))
-        msg, IP = client_udp.recvfrom(1024)
+    while True: #QUANDO FOR VERDADE, OU SEJA, O CHAT NÃO VAI FECHAR SEMPRE VAI FICAR ABERTO LOOP INFINITO
+        client_udp.sendto(raw_input("Digite sua mensagem: "), ("127.0.0.1", 123)) #//ESTAMOS ENVIANDO UMA MENSAGEM, RESPEITANDO 
+        #TUPLA
+        
+        msg, IP = client_udp.recvfrom(1024)# // DIVIDIMOS A SAIDA O PROGRAMA, A MENSAGEM VAI SER A MENSGAGEM E O IP VAI SER O IP DO HOST 
 
-        print "Mensagem: " + msg + "Amigo: " + IP[0] + "\n"
+        print "Mensagem: " + msg + "Amigo: " + IP[0] + "\n"#//ESTAMOS PRINTANDO NA TELA CADA UM DELES E TAMBÉM UTILIZAMOS O INDICE
 
-    client_udp.close()
+    client_udp.close() # QUANDO O PROGRAMA FINALIZA, ELE FECHA
 
 
-except Exception as erro:
-
-    print "Conexão não estabelecida"
+except Exception as erro: #AQUI ELE VAI MOSTRAR O ERRO QUE ESTA OCORRENSDO NO PROGRAMA
+    print erro
+    print "Conexão não estabelecida" #MENSAGEM QUANDO O PROGRAMA NÃO CONSEGUE SE CONECTAR OU DEU PAL
 
     client_udp.close()
