6137e56d76a78553c840b5ff309e7cd4dc30983e
raffaelzzz <34611785+raffaelzzz@users.noreply.github.com>
2019-01-16 15:09:15 -0200

    Create ScanComplet2.py

---
+++ ScanComplet2.py
@@ -0,0 +1,15 @@
+# *-* coding: utf-8 *-*
+
+import socket
+
+
+web = raw_input("Por favor, informe o site que deseja escanear: ")
+ports = [21, 22, 80,3306]
+
+for port in ports:
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.settimeout(0.05)
+    resultado = client.connect_ex((web, port))
+
+    if resultado == 0:
+        print str(port) + " --> Open port"
