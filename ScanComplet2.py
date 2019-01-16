f0b959d332ec576e7f203aba1737b2cf2f570bd5
raffaelzzz <34611785+raffaelzzz@users.noreply.github.com>
2019-01-16 18:11:10 -0200

    Update ScanComplet2.py

--- ScanComplet2.py
+++ ScanComplet2.py
@@ -8,7 +8,7 @@ ports = [80, 21, 22, 443, 8080, 25, 135, 3306] # Defindo as principais portas
 
 for port in ports:
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.settimeout(0.05)
+    client.settimeout(0.05) # podemos mudar para que o usuário decida o tempo
     resultado = client.connect_ex((web, port))
 
     if resultado == 0:
