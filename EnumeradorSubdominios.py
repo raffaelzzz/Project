5784a8af7c7a30e5165dafbcea6b00eda0d857a1
raffaelzzz <34611785+raffaelzzz@users.noreply.github.com>
2019-01-22 14:16:43 -0200

    Update EnumeradorSubdominios.py

--- EnumeradorSubdominios.py
+++ EnumeradorSubdominios.py
@@ -1,1 +1,9 @@
-import dns.resolver
+import dns.resolver #Ele já traz dentro dele a parte louleveu/requisição na parte da rede dele. Baixo nível
+
+resultados = dns.resolver.query('www.mangaking.com.br', 'a') # Estamos fazendo uma requisição, colocamos o site e o tipo 
+
+for resultado in resultados:
+    print resultado
+
+
+
