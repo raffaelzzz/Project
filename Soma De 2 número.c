dea59174c297f01a9eed6ab6dd3b174afeffa3b6
raffaelzzz <34611785+raffaelzzz@users.noreply.github.com>
2019-02-21 09:55:11 -0300

    Create Soma De 2 número.c

---
+++ Soma De 2 número.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+
+    int num1, num2, soma;
+
+    printf("Por favor digite o primeiro numero: \n");
+    scanf("%d", &num1);
+
+    printf("Por favor digite o segundo numero: \n");
+    scanf("%d", &num2);
+
+    soma = num1 + num2;
+
+    printf("a soma é: %d", soma);
+
+
+
+
+}
