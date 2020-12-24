cdb6e52ea7df6bff97850cd4fab1d50031055f17
raffaelzzz <34611785+raffaelzzz@users.noreply.github.com>
2020-12-24 15:20:26 -0500

    Deixando o código mais legível
    Foi modificado o site do alvo, adicionando o autor do programa, arrumando os erros de português.

--- ClientTcp.py
+++ ClientTcp.py
@@ -1,4 +1,13 @@
-# -*- coding: utf-8 -*-
+                                                
+ # -*- coding: utf-8 -*-
+
+##################################################
+#               AUTOR: RAFFAEL BRUNNO            #
+#               DATA: 24.12.2020                 #
+#               PROGAMA: FAZ REQUISIÇÕES HTTP    #
+#                                                #
+##################################################
+
 
 import socket #// Importando o SOCKET
 
@@ -8,16 +17,14 @@ client.settimeout(15) #//O TEMPO QUE O CLIENTE VAI SE CONECTAR COM O SERVIDOR (1
 
 try: #//Então
     
-    client.connect(('www.institutodeandhela.com.br', 80))# //ESTOU DANDO O CONNECT PARA ELE INICIAR, DANDO TAMBÉM OS PARAMENTROS
-    
-    print client.send("GET / HTTP/1.1\nHost: institutodeandhela.com.br\n\n\n")#//A MENSAGEM QUE VAMOS ENVIAR PARA O SERVIDOR
-    
-    print client.recv(1024)# // MENSAGEM QUE VAMOS RECEBER, 1024 BUFFEROVERFLOW
+     client.connect(('hackerdepressivo.com.br', 80))# //ESTOU DANDO O CONNECT PARA ELE INICIAR, DANDO TAMBÉM OS PARAMENTROS
+        
+     print client.send("GET / HTTP/1.1\nHost: hackerdepressivo.com.br\n\n\n")#//A MENSAGEM QUE VAMOS ENVIAR PARA O SERVIDOR
+
+     print client.recv(1024)# // MENSAGEM QUE VAMOS RECEBER, 1024 BUFFEROVERFLOW
 
 except:# // SENÃO
     print "A conexão falhou"
-
-
 
 
 
