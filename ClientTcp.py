c8dbf49f9d0ab0c7b1651cfff18f77e7d86e7c52
raffaelzzz <34611785+raffaelzzz@users.noreply.github.com>
2020-12-24 15:05:35 -0500

    Update ClientTcp.py

--- ClientTcp.py
+++ ClientTcp.py
@@ -4,7 +4,7 @@ import socket #// Importando o SOCKET
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #// CRIANDO O SOCKET COM ESTRUTURA IPV4 e TCP/IP
 
-client.settimeout(15) #//O TEMPO QUE O CLIENTE VAI SE CONECTAR COM O SERVIDOR (15) SE PASSAR DISSO ELE CANCELA
+client.settimeout(15) #//O TEMPO QUE O CLIENTE VAI SE CONECTAR COM O SERVIDOR (15segundos) SE PASSAR DISSO ELE CANCELA
 
 try: #//Então
     
